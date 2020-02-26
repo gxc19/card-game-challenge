20f8e33a31ebc8049dc9d5168fb4a3f6493cb6d7
Ga Ming Chan <gmchan19@gmail.com>
2020-02-26 16:24:48 +0000

    Updates

---
+++ precache-manifest.d6c5fcde678a0adeaf6ba6bfb2b4f948.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f67707676e5cf7d6ac3ff203ece85a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc6880c9bc75e7f34b36",
+    "url": "/static/css/main.1fab05bb.chunk.css"
+  },
+  {
+    "revision": "68632ef75a8df8bd5b74",
+    "url": "/static/js/2.f7e64fa1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f7e64fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6880c9bc75e7f34b36",
+    "url": "/static/js/main.c3c78688.chunk.js"
+  },
+  {
+    "revision": "d62a8d0745e273675d8b",
+    "url": "/static/js/runtime-main.587e4aae.js"
+  },
+  {
+    "revision": "bcdbf223dd3c2f13bef19d97d72c5058",
+    "url": "/static/media/babymario.bcdbf223.png"
+  },
+  {
+    "revision": "b296e1635e0f9cbc5ca10643dab29466",
+    "url": "/static/media/bowser.b296e163.png"
+  },
+  {
+    "revision": "416fae70576a8fcb296e6e8248b7dbad",
+    "url": "/static/media/bowserjr.416fae70.png"
+  },
+  {
+    "revision": "d37cf451f559a5806837b673dbeb3880",
+    "url": "/static/media/donkeykong.d37cf451.png"
+  },
+  {
+    "revision": "7d1fcf1e1913af06af5770bd87b11480",
+    "url": "/static/media/kingboo.7d1fcf1e.png"
+  },
+  {
+    "revision": "dd30d2c5df7f4173cc42286168b31078",
+    "url": "/static/media/morton.dd30d2c5.png"
+  },
+  {
+    "revision": "e3104c45c66e22f1d67c651461996551",
+    "url": "/static/media/refresh.e3104c45.png"
+  },
+  {
+    "revision": "b70c7b5f47f0da3c4a0a8a9a4c915c7b",
+    "url": "/static/media/shyguy.b70c7b5f.png"
+  },
+  {
+    "revision": "79686ed9d9d573256e5096b41916b853",
+    "url": "/static/media/waluigi.79686ed9.png"
+  }
+]);
